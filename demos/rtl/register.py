cf2693230ee868dff549e5baf533e7ad571f08ec
TRIFOGLIOA <alessandro.trifoglio@leonardo.com>
2026-01-21 14:45:51 +0100

    update generate_code call in register.py to reflect the new signature

--- demos/rtl/register.py
+++ demos/rtl/register.py
@@ -107,4 +107,4 @@ class Testbench(Part):
 if __name__ == "__main__":
     simulate(Testbench('tb'), 0.1, 3.0)
     # view_diagram(Testbench('tb'))
-    # generate_code(Register('dut'), "VHDL", "gen/register", "register", "rtl", llm=True, generate_build_script=True)
+    # generate_code(Register('dut'), "VHDL", "gen/register", "register", "rtl", llm=True, generate_build_script=True, generate_purge_script=True)
